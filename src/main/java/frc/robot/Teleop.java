f1749e57f20a1ce11a818484d560c0a47226e25f
unknown <acjparadis@outlook.com>
2019-02-09 14:04:02 -0500

    File transfer between ATA and Swat repos

--- src/main/java/frc/robot/Teleop.java
+++ src/main/java/frc/robot/Teleop.java
@@ -13,38 +13,31 @@ public class Teleop {
 
     private Joystick gunnerStick = new Joystick(1);
     private Joystick driveStick = new Joystick(0);
-    private SWATDrive driveTrain;
+    private Drive drive = new Drive();
     private Intake intake;
     public Teleop() {
-        //Drive Controllers
-        WPI_VictorSPX rearLeftMotor = new WPI_VictorSPX(1);
-        WPI_TalonSRX frontLeftMotor = new WPI_TalonSRX(0);
-        WPI_VictorSPX rearRightMotor = new WPI_VictorSPX(3);
-        WPI_TalonSRX frontRightMotor = new WPI_TalonSRX(2);
-        //Group Drive
-        SpeedControllerGroup rightMotors = new SpeedControllerGroup(rearRightMotor, frontRightMotor);
-        SpeedControllerGroup leftMotors = new SpeedControllerGroup(rearLeftMotor, frontLeftMotor);
         //Add pneumatics
         DoubleSolenoid hatchIntakeSolenoid = new DoubleSolenoid(0, 1);  
         DoubleSolenoid m_gearShiftSolenoid = new DoubleSolenoid(2, 3);  
-        driveTrain = new SWATDrive(leftMotors, rightMotors, m_gearShiftSolenoid);
         intake = new Intake(hatchIntakeSolenoid);
     }
     public void init() {
         intake.hatchOff();
     }
-
-
+    public void TeleopInit() {
+      
+    }
+  
     public void TeleopPeriodic() {
-          driveTrain.arcadeDrive(driveStick.getRawAxis(1), driveStick.getRawAxis(4), true);
-          if(driveStick.getRawButton(3)) {
-            driveTrain.gearShift();
-          }
-        intake.IntakePeriodic(gunnerStick.getRawButton(5), gunnerStick.getRawButton(6));
-          if (driveStick.getRawButton(1)) {
-            driveTrain.slow = !driveTrain.slow;
-          } 
-          driveTrain.arcadeDrive(driveStick.getRawAxis(1), driveStick.getRawAxis(4), true);
-
+      drive.drive(driveStick);
+      
+      if(driveStick.getRawButton(3)) {
+        drive.gearShift();
+      }
+    }
+  
+    public void ExtDrive(double leftValue, double rightValue) {
+      drive.ExtDrive(leftValue, rightValue);
     }
 }
+  
